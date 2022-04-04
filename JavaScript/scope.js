d629951c61ed64b03ed9e06bc95b5d82950d1e41
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2022-04-04 16:44:12 +0900

    Error update

--- JavaScript/scope.js
+++ JavaScript/scope.js
@@ -33,12 +33,11 @@ bar();
 
 (function () {
   var foo = 10;
-
-}());
+})();
 
 // 전역 변수 억제방법중 네임스페이스 객체를 사용해서 하는방법
 
-ver MYAPP = {} // 전역 스페이스 객체 
+var MYAPP = {}; // 전역 스페이스 객체
 
 MYAPP.person = {
   name: "Lee",
@@ -47,10 +46,9 @@ MYAPP.person = {
 
 console.log(MYAPP.person.name); // lee
 
+// 모듈화
 
-// 모듈화 
-
-var Counter = (function () {
+var Counter = function () {
   var num = 0;
 
   return {
@@ -59,21 +57,19 @@ var Counter = (function () {
     },
     decrease() {
       return --num;
-    }
+    },
   };
-});
+};
 // private 변수는 외부로노출이 되지않음
-console.log(Counter.num) // undefined
+console.log(Counter.num); // undefined
 
-console.log(Counter.incresase()) // 1
-console.log(Counter.incresase()) // 2
-console.log(Counter.decrease()) // 1
-console.log(Counter.decrease()) // 0
+console.log(Counter.incresase()); // 1
+console.log(Counter.incresase()); // 2
+console.log(Counter.decrease()); // 1
+console.log(Counter.decrease()); // 0
 
 // 모듈화한 메소드를 통해서 Counter 안에 변수를 조작할수있다.
 
 //ES6 모듈
 
-  < script type = "module" src = "lib.mjs" ></ >
-
-    
+// < script type = "module" src = "lib.mjs" ></ >
