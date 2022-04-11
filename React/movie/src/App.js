5e080beca951fef5ed77dce3e9cd207681df969e
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2022-04-11 20:57:28 +0900

    2022-04-11

---
+++ React/movie/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+      <div>
+          <h1>무비</h1>
+          <select>
+              <option>xx</option>
+              <option>xx</option>
+              <option>xx</option>
+          </select>
+    </div>
+  );
+}
+
+export default App;
